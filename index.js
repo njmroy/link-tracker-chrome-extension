c8cd32088e8d8a1a4274eb8b0318f03c8ffce998
Nicolas Roy <37703450+njmroy@users.noreply.github.com>
2022-05-08 09:47:30 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+let myLeads = []
+
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value);
+    renderLeads();
+    inputEl.value = "";
+})
+
+function renderLeads() { 
+    let listItems = "";
+    for (let i=0; i<myLeads.length;i++) {
+        //ulEl.innerHTML += `<li> ${myLeads[i]} </li>`;
+        listItems += `
+        <li>
+            <a target="_blank" href="${myLeads[i]}"> ${myLeads[i]} </a> 
+        </li>
+        `;
+        //Same method as above but split up. 
+        //Create new li element 
+        // makes <li></li>
+        //const li = document.createElement("li");
+
+        //Add leads as text content of newly made list element
+        //makes <li> myLeads[i] </li>
+        //li.textContent = myLeads[i];
+
+        //Add list item to unordered list element 
+        //Connects <li> myLeads[i] </li> to unordered list element
+        //ulEl.append(li);
+
+    }
+    //DOM manipulation has a cost and should be done outside of loops. 
+    ulEl.innerHTML = listItems;
+
+}
