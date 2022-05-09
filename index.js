e31cb447a58efabe07a0cd12e2af50d64abecee0
Nicolas Roy <37703450+njmroy@users.noreply.github.com>
2022-05-09 09:05:26 -0500

    added local Storage

--- index.js
+++ index.js
@@ -7,9 +7,20 @@ const ulEl = document.getElementById("ul-el")
 localStorage.setItem("myLeads", "www.example.com")
 
 inputBtn.addEventListener("click", function() {
+    //add input field value to myLeads array
     myLeads.push(inputEl.value);
-    renderLeads();
+    //Clear input field
     inputEl.value = "";
+    //save leads to localStorage
+    //convert to JSON string format
+    myLeads = JSON.stringify(myLeads);
+    //add to local storage
+    localStorage.setItem(myLeads);
+    //log item in console
+    console.log(localStorage.getItem("myLeads"));
+    
+    renderLeads();
+    
 })
 
 function renderLeads() { 
@@ -29,7 +40,7 @@ function renderLeads() {
         //Add leads as text content of newly made list element
         //makes <li> myLeads[i] </li>
         //li.textContent = myLeads[i];
-
+ß
         //Add list item to unordered list element 
         //Connects <li> myLeads[i] </li> to unordered list element
         //ulEl.append(li);
